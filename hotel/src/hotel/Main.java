1b7c93777671c0522e08d0548af4bad9eb0fdad4
bieitoSousa <35741016+bieitoSousa@users.noreply.github.com>
2019-10-25 02:13:51 +0200

    integrado libreria qt

--- hotel/src/hotel/Main.java
+++ hotel/src/hotel/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         QApplication.initialize(args);
                 QMainWindow mw = new QMainWindow();
                 frmPrincipal principal = new frmPrincipal();
